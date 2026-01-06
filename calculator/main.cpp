37f69f2d5cb8cc8efa57587e5c3a4c35f537617f
Nikita <100510nik@gmail.com>
2026-01-06 15:14:54 +0300

    Update main.cpp

--- calculator/main.cpp
+++ calculator/main.cpp
@@ -5,15 +5,39 @@
 
 int main(int argc, char *argv[]){
     QApplication a(argc, argv);
-    CalcMainWindow window(nullptr);
-    window.resize(400, 500);
-    window.move(400, 300);
 
-    Ui::MainWindow calc;
-    calc.setupUi(&window);
+    CalcMainWindow calc;
+    calc.resize(200, 300);
+    calc.move(300, 300);
 
-    window.lineEdit = calc.lineEdit;
+    Ui::MainWindow ui;
+    ui.setupUi(&calc);
 
-    window.show();
+    calc.lineEdit = ui.lineEdit;
+
+    QList<QPushButton*> digitButtons = {
+        ui.pushButton_0, ui.pushButton_1, ui.pushButton_2, ui.pushButton_3,
+        ui.pushButton_4, ui.pushButton_5, ui.pushButton_6, ui.pushButton_7,
+        ui.pushButton_8, ui.pushButton_9
+    };
+
+    for(QPushButton *btn : digitButtons) {
+            QObject::connect(btn, &QPushButton::clicked, &calc, &CalcMainWindow::clickedDigit);
+        }
+
+        QList<QPushButton*> opButtons = {
+            ui.pushButton_plus, ui.pushButton_minus, ui.pushButton_mul,
+            ui.pushButton_div, ui.pushButton_sqr, ui.pushButton_root, ui.pushButton_procent, ui.pushButton_dot
+        };
+
+        for(QPushButton *btn : opButtons) {
+            QObject::connect(btn, &QPushButton::clicked, &calc, &CalcMainWindow::clickedOperator);
+        }
+
+        QObject::connect(ui.pushButton_equal, &QPushButton::clicked, &calc, &CalcMainWindow::equality);
+        QObject::connect(ui.pushButton_clear, &QPushButton::clicked, &calc, &CalcMainWindow::delete_all);
+
+
+    calc.show();
     return a.exec();
 }
